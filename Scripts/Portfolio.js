5f1aef265f1194168f2b56bef82439abbc354606
tejhande <59686002+tejhande@users.noreply.github.com>
2024-06-04 00:34:34 +0530

    "Added 'Go to Top' button functionality with smooth scrolling animation"

--- Scripts/Portfolio.js
+++ Scripts/Portfolio.js
@@ -131,6 +131,38 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+
+
+// Get the button
+const goTopBtn = document.getElementById("goTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goTopBtn.style.display = "block";
+  } else {
+    goTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document with smooth animation
+goTopBtn.addEventListener("click", smoothScrollToTop);
+
+function smoothScrollToTop() {
+  const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(smoothScrollToTop);
+    window.scrollTo(0, currentScroll - (currentScroll / 10));
+  }
+}
+
+
+
 // Tejas Hande
 // tejasamolhande@gmail.com
 // 8600828734
